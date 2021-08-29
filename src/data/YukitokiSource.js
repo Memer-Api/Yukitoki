111222a3e2fdc683a12f242fac44f967087519f8
Yash <67926590+Yash094@users.noreply.github.com>
2021-08-29 21:39:18 +0530

    Update YukitokiSource.js

--- src/data/YukitokiSource.js
+++ src/data/YukitokiSource.js
@@ -1,6 +1,6 @@
 import DocsManager from "./DocsManager";
 
-const blacklisted = new Set(["docs"]);
+const blacklisted = new Set(["docs", "v3.1.0@beta", "v3.0.7" ,"v3.0.0@beta" ,"v1" ,"v2","v3.0.7", "v2.2.1" ]);
 
 export default new DocsManager({
     id: "memer",

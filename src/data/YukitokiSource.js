a524a7f977854f72fd673bc093c2e3bc4ecbec69
Yash <67926590+Yash094@users.noreply.github.com>
2021-08-29 21:33:16 +0530

    Update YukitokiSource.js

--- src/data/YukitokiSource.js
+++ src/data/YukitokiSource.js
@@ -3,10 +3,10 @@ import DocsManager from "./DocsManager";
 const blacklisted = new Set(["docs"]);
 
 export default new DocsManager({
-    id: "yukitoki",
+    id: "memer",
     name: "Main",
-    global: "Yukitoki",
-    repo: "DevSnowflake/yukitoki-docs",
+    global: "Memer",
+    repo: "Yash094/memer-api",
     defaultTag: "main",
     docsBranch: "docs",
     branchFilter: (branch) => !blacklisted.has(branch) && !branch.startsWith("dependabot/")
